3c540ae7e0fc3957e1201976d0c592de92ba0c03
a.flore <a.flore@octavianlab.com>
2022-03-15 17:33:57 +0100

    add custom icon marker

--- src/components/LocationMap.js
+++ src/components/LocationMap.js
@@ -1,14 +1,45 @@
+import { useEffect, useState } from "react";
 import { GMap } from "primereact/gmap";
+import { loadGoogleMaps, removeGoogleMaps } from "../js/GoogleMaps";
 
 const LocationMap = () => {
+  const [googleMapsReady, setGoogleMapsReady] = useState(false);
+  const [overlays, setOverlays] = useState(null);
+  const google = window.google;
   const options = {
     center: { lat: 40.733614, lng: 17.579342 },
     zoom: 17,
   };
+  const onMapReady = (event) => {
+    setOverlays([
+      new google.maps.Marker({
+        position: { lat: 40.734186, lng: 17.579608 },
+        title: "Red house",
+        icon: "https://img.icons8.com/flat-round/40/000000/home--v1.png",
+      }),
+    ]);
+  };
+  useEffect(() => {
+    loadGoogleMaps(() => {
+      setGoogleMapsReady(true);
+    });
 
+    return () => {
+      removeGoogleMaps();
+    };
+  }, []);
 
   return (
-    <GMap options={options} style={{ width: "100%", minHeight: "320px" }} />
+    <div>
+      {googleMapsReady && (
+        <GMap
+          overlays={overlays}
+          onMapReady={onMapReady}
+          options={options}
+          style={{ width: "100%", minHeight: "320px" }}
+        />
+      )}
+    </div>
   );
 };
 
