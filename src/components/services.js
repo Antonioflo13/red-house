79693c9f35f3b5a1efb32fe8af3edc1c3c7df08c
Antonioflo13 <floreantonio@hotmail.it>
2022-03-30 08:56:04 +0200

    add fontawesome

--- src/components/services.js
+++ src/components/services.js
@@ -1,9 +1,32 @@
 import { Card } from "primereact/card";
 import LocationMap from "./LocationMap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faKitchenSet, faTv, faWifi } from "@fortawesome/free-solid-svg-icons";
 
 const Services = () => {
   return (
     <Card title="I nostri servizi" className={`shadow-8`}>
+      <div>
+        <h3>Cosa troverai</h3>
+        <div>
+          <FontAwesomeIcon icon={faKitchenSet} className="mr-2" />
+          Cucina
+        </div>
+        <div>Asciugacapelli</div>
+        <div>
+          <FontAwesomeIcon icon="fa-solid fa-air-conditioner" />
+          Forno
+        </div>
+        <div>
+          <FontAwesomeIcon icon={faWifi} className="mr-2" />
+          Wi-fi
+        </div>
+        <div>
+          <FontAwesomeIcon icon={faTv} className="mr-2" />
+          TV
+        </div>
+        <div>Frigorifero</div>
+      </div>
       <LocationMap />
     </Card>
   );
